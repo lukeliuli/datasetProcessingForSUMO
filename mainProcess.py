60018c2164459baf7121550d3d1c423f70641569
lukeliuli <lukeliuli@163.com>
2022-04-05 17:23:23 +0800

    show traces ok

--- mainProcess.py
+++ mainProcess.py
@@ -16,17 +16,33 @@ print("#################################################################")
 #print("veh_lane.unique()\n",veh_lane.unique())
 print("#################################################################")
 
-for i,data in enumerate(veh_lane.unique()):
-    if i==2:
-        break
-    #print(i,data)
+print("#################################################################")
+print("显示多车同一车道")
+print("#################################################################")
+
+
+df1=df.groupby(["vehicle_lane","timestep_time"]).size()
+#print(df1)
+col=df1[df1>1].reset_index()[["vehicle_lane","timestep_time"]]
+#print(col)
+mulVehsOneLane = pd.merge(col,df,on=["vehicle_lane","timestep_time"])
+print(mulVehsOneLane)
+input()
+
+print("#################################################################")
+print("显示轨迹")
+print("#################################################################")
+df = mulVehsOneLane
+for i,data in enumerate(df.vehicle_lane.unique()):
+    
     vehInOneLane = df[df.vehicle_lane==data]
     #print(vehInOneLane)
     lanePosX =  vehInOneLane.vehicle_x
     lanePosY =  vehInOneLane.vehicle_y
     maxTime = round(max(vehInOneLane.timestep_time))
+    minTime = round(min(vehInOneLane.timestep_time))
     
-    for t in range(maxTime):
+    for t in range(minTime,maxTime):
         tmp  = vehInOneLane.vehicle_id.unique()
         print(t)
         #print(lanePosX)
@@ -35,6 +51,7 @@ for i,data in enumerate(veh_lane.unique()):
         plt.figure(1)
         plt.plot( lanePosX,lanePosY, '.',color = 'black',linewidth=1,markersize=0.1,label='lane'),
         colorList= ['red','green','blue','yellow']
+        title = "time:"+str(t)+";"
         for ii,data2  in enumerate(tmp):
             #print(t)
             veh = vehInOneLane[(vehInOneLane.vehicle_id == data2) & (vehInOneLane.timestep_time == float(t))] 
@@ -44,15 +61,16 @@ for i,data in enumerate(veh_lane.unique()):
             
             vehX =  veh.vehicle_x
             vehY =  veh.vehicle_y
-            vehVel = veh.vehicle_speed
+            vehVel = veh.vehicle_speed.to_numpy()
             vehTime  = veh.timestep_time
             
-            
+            title = title+"\n"+data2+",Speed:"+str(vehVel)+";"
             plt.plot( vehX,vehY, 'o',color= colorList[ii%4],label=data2)  # 蓝色圆点实线 
             plt.legend()
-            
+        
+        plt.title(title)    
         plt.show()
-        plt.pause(0.001)
+        plt.pause(1)
         plt.ioff()
         plt.clf()
         
