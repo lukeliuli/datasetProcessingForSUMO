e290ea094a0b1c2f9106f311f70fa3ed52a459aa
lukeliuli <lukeliuli@163.com>
2022-04-05 13:47:10 +0800

    temp saving ,error

--- mainProcess.py
+++ mainProcess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('vanet-trace-creteil-20130924-0700-0900.csv',sep = ';')
+df = pd.read_csv('data1.csv',sep = ';')
 print("#################################################################")
 print("df.head()\n",df.head())
 #print("df.info()\n",df.info())
@@ -12,7 +12,7 @@ print("veh_lane1\n",veh_lane1)
 
 veh_lane = df.vehicle_lane
 print("#################################################################")
-print("veh_lane.unique()\n",veh_lane.unique())
+#print("veh_lane.unique()\n",veh_lane.unique())
 print("#################################################################")
 
 for i,data in enumerate(veh_lane.unique()):
@@ -21,11 +21,34 @@ for i,data in enumerate(veh_lane.unique()):
     #print(vehInOneLane)
     lanePosX =  vehInOneLane.vehicle_x
     lanePosY =  vehInOneLane.vehicle_y
-    #print(lanePosX)
-    plt.plot( lanePosX,lanePosY, '.')  # 蓝色圆点实线
+    maxTime = round(max(vehInOneLane.timestep_time))
+    
+    for t in range(maxTime):git 
+        tmp  = vehInOneLane.vehicle_id.unique()
+        print(len(tmp))
+        #print(lanePosX)
+        #plt.plot( lanePosX,lanePosY, '.')  # 蓝色圆点实线
+        
+        for data2  in tmp:
+            print(data2)
+            veh = vehInOneLane[vehInOneLane.vehicle_id==tmp & vehInOneLane.timestep_time == t]
+            if veh == None:
+                continue
+            
+            vehX =  veh.vehicle_x
+            vehY =  veh.vehicle_y
+            vehVel = veh.vehicle_speed
+            vehTime  = veh.timestep_time
+            plt.plot( lanePosX,lanePosY, '.')
+            plt.plot( vehX,vehY, 'o')  # 蓝色圆点实线 
+            plt.show()
+        
+
+    break
     
 plt.show()
 
 
 #plt.plot(df.vehicle_x,df.vehicle_y, '.')  # 蓝色圆点实线
 #plt.show()
+
